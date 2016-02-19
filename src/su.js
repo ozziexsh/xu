22bf5d90ad0c55f9bef6ce1b9971ff22c04895be
Ozzie Neher <oz.neher@gmail.com>
2016-02-18 18:13:01 -0600

    Added base functions

--- src/su.js
+++ src/su.js
@@ -8,5 +8,31 @@
     }
   }
 
+  su.prototype.css = function(styles) {
+    for (var property in styles) {
+      this.el.style[property] = styles[property];
+    }
+  };
+
+  su.prototype.setClass = function(cl) {
+    this.el.className = cl;
+  };
+
+  su.prototype.appendClass = function(cl) {
+    this.el.className += ' ' + cl;
+  };
+
+  su.prototype.hasClass = function(cl) {
+    if (this.el.className.indexOf(cl) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  su.prototype.on = function(event, callback) {
+    this.el.addEventListener(event, callback);
+  };
+
   module.exports = su;
 })();
