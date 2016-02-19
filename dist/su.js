2a8f4f4e8f48e7fa75e79c86c76822fa19b462e7
Ozzie Neher <oz.neher@gmail.com>
2016-02-19 09:32:30 -0600

    Updated build

--- dist/su.js
+++ dist/su.js
@@ -1,12 +1,52 @@
 (function() {
-  function su(selector) {
+
+  window.su = function su(selector) {
     // Allows calling of su without the 'new' keyword
     if (this instanceof su) {
       this.el = document.querySelector(selector);
     } else {
       return new su(selector);
     }
-  }
+  };
 
-  module.exports = su;
+  su.prototype.css = function(styles) {
+    for (var property in styles) {
+      this.el.style[property] = styles[property];
+    }
+  };
+
+  su.prototype.setClass = function(cl) {
+    this.el.className = cl;
+  };
+
+  su.prototype.appendClass = function(cl) {
+    this.el.className += ' ' + cl;
+  };
+
+  su.prototype.hasClass = function(cl) {
+    if (this.el.className.indexOf(cl) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  su.prototype.on = function(event, callback) {
+    this.el.addEventListener(event, callback);
+  };
+
+  su.prototype.attr = function(attributes) {
+    // Getter
+    if (typeof attributes == 'string') {
+      return this.el[attributes];
+    }
+    // Setter
+    for (var attribute in attributes) {
+      this.el[attribute] = attributes[attribute];
+    }
+  };
+
+  su.prototype.text = function(text) {
+    this.el.innerHTML = text;
+  };
 })();
