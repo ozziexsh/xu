cbe279e76e79ab3516e4aec7cb18c8011a7a62bc
Selexin <jacobhmorris@gmail.com>
2016-02-26 17:23:39 +1100

    Code style update - remove spaces in for loops

--- src/su.js
+++ src/su.js
@@ -9,7 +9,7 @@
   };
 
   su.prototype.css = function(styles) {
-    for(var i=0; i<this.el[i].length;i++) {
+    for(var i=0;i<this.el[i].length;i++) {
       for (var property in styles) {
         this.el[i].style[property] = styles[property];
       }
@@ -17,19 +17,19 @@
   };
 
   su.prototype.setClass = function(cl) {
-    for(var i=0; i<this.el[i].length;i++) {
+    for(var i=0;i<this.el[i].length;i++) {
       this.el[i].className = cl;
     }
   };
 
   su.prototype.appendClass = function(cl) {
-    for(var i=0; i<this.el[i].length;i++) {
+    for(var i=0;i<this.el[i].length;i++) {
       this.el[i].className += ' ' + cl;
     }
   };
 
   su.prototype.hasClass = function(cl) {
-    for(var i=0; i<this.el[i].length;i++) {
+    for(var i=0;i<this.el[i].length;i++) {
       if (this.el[i].className.indexOf(cl) == -1) {
         return false; // Immediately return, no class found in the set of matched objects.
       }
@@ -38,7 +38,7 @@
   };
 
   su.prototype.on = function(event, callback) {
-    for(var i=0; i<this.el[i].length;i++) {
+    for(var i=0;i<this.el[i].length;i++) {
       this.el[i].addEventListener(event, callback);
     }
   };
@@ -46,20 +46,20 @@
   su.prototype.attr = function(attributes) {
     // Getter
     if (typeof attributes == 'string') {
-      for(var i=0; i<this.el[i].length;i++) {
+      for(var i=0;i<this.el[i].length;i++) {
         return this.el[i][attributes];
       }
     }
     // Setter
     for (var attribute in attributes) {
-      for(var i=0; i<this.el[i].length;i++) {
+      for(var i=0;i<this.el[i].length;i++) {
         this.el[i][attribute] = attributes[attribute];
       }
     }
   };
 
   su.prototype.text = function(text) {
-    for(var i=0; i<this.el[i].length;i++) {
+    for(var i=0;i<this.el[i].length;i++) {
       this.el[i].innerHTML = text;
     }
   };
